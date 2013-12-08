77012a40db83f8f1969d15166cecd9b102e3d9aa
sh1989 <samhogy@gmail.com>
2013-12-08 19:00:43 +0000

    RailwayLines enum holds resource ids

--- MetroAppWidget/src/uk/co/samhogy/metroappwidget/model/RailwayLines.java
+++ MetroAppWidget/src/uk/co/samhogy/metroappwidget/model/RailwayLines.java
@@ -1,8 +1,16 @@
 
 package uk.co.samhogy.metroappwidget.model;
 
+import uk.co.samhogy.metroappwidget.R;
+
 public enum RailwayLines {
-    GREEN,
-    YELLOW,
-    ALL
+    GREEN (R.color.line_green),
+    YELLOW (R.color.line_yellow),
+    ALL (R.drawable.shape_all_lines);
+
+    final int drawableResourceId;
+
+    RailwayLines (int drawableResourceId) {
+        this.drawableResourceId = drawableResourceId;
+    }
 }
